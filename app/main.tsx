20a36f7b6c83ae8c6915fb92e0bb5e545c20317e
Durgarao310 <gantadurgarao304@gmail.com>
2026-05-06 12:20:07 +0530

    Update asset references in index.html and service worker for new build

--- app/main.tsx
+++ app/main.tsx
@@ -6,7 +6,6 @@ import { RouterProvider } from "react-router"
 import { router } from "./router"
 import { store } from "./store"
 import "./app.css"
-import { RouterProvider } from "react-router"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
