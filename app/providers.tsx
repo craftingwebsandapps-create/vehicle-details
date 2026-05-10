dd190696961905156d996f3c851042e55fa44484
Durgarao310 <gantadurgarao304@gmail.com>
2026-05-10 14:15:42 +0530

    feat: wrap application components with TooltipProvider in AppProviders

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,16 @@ import type { ReactNode } from "react"
 import { Provider } from "react-redux"
 
 import { store } from "~/app/store"
+import { TooltipProvider } from "~/components/ui/tooltip"
 
 type AppProvidersProps = {
   children: ReactNode
 }
 
 export function AppProviders({ children }: AppProvidersProps) {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <TooltipProvider>{children}</TooltipProvider>
+    </Provider>
+  )
 }
